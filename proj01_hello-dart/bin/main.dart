ae5686ccd397ef12c6b56354ff0cc4afd1ea4f5c
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-05-11 20:11:42 +0100

    8. Setup Dart and Flutter and IntelliJ IDE on Mac

---
+++ proj01_hello-dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:proj01_hello_dart/proj01_hello_dart.dart' as proj01_hello_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${proj01_hello_dart.calculate()}!');
+}
---
+++ proj01_hello-dart/test/proj01_hello_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:proj01_hello_dart/proj01_hello_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
